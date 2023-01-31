50c3838224e389f823aceba9de2f05c865445476
Мария Таскина <maryyysh2016@mail.ru>
2023-01-31 23:56:07 +0300

    step 6 rev.3 add Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,32 @@
+/*
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Engine {
+    public static void startGames() {
+        Scanner user = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = user.next();
+        System.out.println("Hello, " + userName + "!");
+
+        Scanner number = new Scanner(System.in);
+        int count = 0;
+        for (var i = 0; i < 3; i++) {
+            System.out.println("Question: " + );
+            System.out.print("Your answer: ");
+            String userAnswer = number.next().toLowerCase();
+            if () {
+                System.out.println("Correct!");
+                count++;
+            } else if () {
+                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '" + "'. \nLet's try again, " + userName + "!");
+                break;
+            }
+        }
+        if (count == 3) {
+            System.out.println("Congratulations, " + userName + "!");
+        }
+    }
+}
+ */
