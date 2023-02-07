2c773d3f9a987b34d760d91bb1b71d199cb81d72
Мария Таскина <maryyysh2016@mail.ru>
2023-02-07 20:58:39 +0300

    update Randomizer.java

--- app/src/main/java/hexlet/code/Randomizer.java
+++ app/src/main/java/hexlet/code/Randomizer.java
@@ -2,6 +2,6 @@ package hexlet.code;
 
 public class Randomizer {
     public static int getRandom() {
-        return (int) (Math.random() * 100 + 1);
+        return (int) (Math.random() * 10 + 1);
     }
 }
