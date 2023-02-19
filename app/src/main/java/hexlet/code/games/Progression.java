150914d44480bd71ca9c81ec40071bc215c2581b
Мария Таскина <maryyysh2016@mail.ru>
2023-02-19 11:30:02 +0300

    fix Line is longer than 120 characters

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -10,25 +10,25 @@ public class Progression {
     private static final int MAX_ROUNDS = 3;
     static final int MIN_LENGTH_OF_PROGRESSION = 5;
     static final int MAX_LENGTH_OF_PROGRESSION = 10;
-    public static String[] createMassive(int firstNumber, int differenceBetweenTwoNumbers, char randomOperation, int length) {
+    public static String[] createMassive(int firstNumber, int diffBetweenTwoNum, char randomOperation, int length) {
         String[] strMassive = new String[length];
-        int[] intMassive = new int[length];
-        intMassive[0] = firstNumber;
-        for (var i = 1; i < intMassive.length; i++) {
+        int[] intMas = new int[length];
+        intMas[0] = firstNumber;
+        for (var i = 1; i < intMas.length; i++) {
             if (randomOperation == '-') {
-                intMassive[i] = intMassive[i - 1] - differenceBetweenTwoNumbers;
+                intMas[i] = intMas[i - 1] - diffBetweenTwoNum;
             } else if (randomOperation == '+') {
-                intMassive[i] = intMassive[i - 1] + differenceBetweenTwoNumbers;
+                intMas[i] = intMas[i - 1] + diffBetweenTwoNum;
             }
         }
         for (var j = 0; j < strMassive.length; j++) {
-            strMassive[j] = String.valueOf(intMassive[j]);
+            strMassive[j] = String.valueOf(intMas[j]);
         }
         return strMassive;
     }
-    public static String massiveWithHiddenSpot(String[] intMassive, int hiddenSpot) {
-        intMassive[hiddenSpot] = "..";
-        String stringWithHiddenSpot = String.join(" ", intMassive);
+    public static String massiveWithHiddenSpot(String[] intMas, int hiddenSpot) {
+        intMas[hiddenSpot] = "..";
+        String stringWithHiddenSpot = String.join(" ", intMas);
         return stringWithHiddenSpot;
     }
     public static void fillTheBlank() {
@@ -44,14 +44,14 @@ public class Progression {
         int count = 0;
         for (var i = 0; i < MAX_ROUNDS; i++) {
             int firstNumber = Randomizer.getRandom();
-            int differenceBetweenTwoNumbers = Randomizer.getRandom();
+            int diffBetweenTwoNum = Randomizer.getRandom();
             var randomOperation = RandomOperationForProgression.getRandomOperationForProgression();
             int lengthOfProgression = Randomizer.getRandom(MIN_LENGTH_OF_PROGRESSION, MAX_LENGTH_OF_PROGRESSION);
-            int hiddenSpotIndex = lengthOfProgression - 2;
-            String[] intMassive = createMassive(firstNumber, differenceBetweenTwoNumbers, randomOperation, lengthOfProgression);
-            int secretNumber = (Integer.parseInt(intMassive[hiddenSpotIndex + 1]) + Integer.parseInt(intMassive[hiddenSpotIndex - 1])) / 2;
+            int hidIndex = lengthOfProgression - 2;
+            String[] intMas = createMassive(firstNumber, diffBetweenTwoNum, randomOperation, lengthOfProgression);
+            int secretNumber = (Integer.parseInt(intMas[hidIndex + 1]) + Integer.parseInt(intMas[hidIndex - 1])) / 2;
             String stringSecretNumber = Integer.toString(secretNumber);
-            System.out.println("Question: " + massiveWithHiddenSpot(intMassive, hiddenSpotIndex));
+            System.out.println("Question: " + massiveWithHiddenSpot(intMas, hidIndex));
             System.out.print("Your answer: ");
             String userAnswer = number.next();
             if (userAnswer.equals(stringSecretNumber)) {
