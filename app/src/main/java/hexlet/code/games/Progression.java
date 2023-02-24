19c095b9248541c87cfb91488107b09c38af6286
Мария Таскина <maryyysh2016@mail.ru>
2023-02-24 20:50:54 +0300

    fix 14th remark

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -10,7 +10,7 @@ import static hexlet.code.Engine.ANSWER_PLUS_QUESTION;
 public class Progression {
     static final int MIN_LENGTH_OF_PROGRESSION = 5;
     static final int MAX_LENGTH_OF_PROGRESSION = 10;
-    public static String[] createMassive(int firstNumber, int diffBetweenTwoNum, char randomOperation, int length) {
+    public static String[] generateProgression(int firstNumber, int diffBetweenTwoNum, char randomOperation, int length) {
         String[] strMassive = new String[length];
         int[] intMas = new int[length];
         intMas[0] = firstNumber;
@@ -41,7 +41,7 @@ public class Progression {
             var randomOperation = RandomOperationForProgression.getRandomOperationForProgression();
             int lengthOfProgression = Randomizer.getRandom(MIN_LENGTH_OF_PROGRESSION, MAX_LENGTH_OF_PROGRESSION);
             int hidIndex = lengthOfProgression - 2;
-            String[] intMas = createMassive(firstNumber, diffBetweenTwoNum, randomOperation, lengthOfProgression);
+            String[] intMas = generateProgression(firstNumber, diffBetweenTwoNum, randomOperation, lengthOfProgression);
             int secretNumber = (Integer.parseInt(intMas[hidIndex + 1]) + Integer.parseInt(intMas[hidIndex - 1])) / 2;
             String stringSecretNumber = Integer.toString(secretNumber);
 
