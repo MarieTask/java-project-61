99ef62e2daccdb5c41d41f9301d997b9433488c2
Мария Таскина <maryyysh2016@mail.ru>
2023-02-24 21:10:27 +0300

    fix 15th remark rev.2

--- app/src/main/java/hexlet/code/RandomOperation.java
+++ app/src/main/java/hexlet/code/RandomOperation.java
@@ -1,16 +1,12 @@
 package hexlet.code;
 
-import java.util.Random;
-
 public class RandomOperation {
     public static final char[] SYMBOLS = {'+', '-', '*'};
     public static final char[] SYMBOLSFORPROGRESSION = {'+', '-'};
     public static char getRandomOperation() {
-        char operation = SYMBOLS[Randomizer.getRandom(0, 2)];
-        return operation;
+        return SYMBOLS[Randomizer.getRandom(0, 2)];
     }
     public static char getRandomOperationForProgression() {
-        char operationForProgression = SYMBOLSFORPROGRESSION[Randomizer.getRandom(0, 1)];
-        return operationForProgression;
+        return SYMBOLSFORPROGRESSION[Randomizer.getRandom(0, 1)];
     }
 }
