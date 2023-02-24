dd1ae3885643e12c0eda886d2dcfb6cd46d4841e
Мария Таскина <maryyysh2016@mail.ru>
2023-02-24 19:42:15 +0300

    fix 6th remark rev.2

--- app/src/main/java/hexlet/code/RandomOperation.java
+++ app/src/main/java/hexlet/code/RandomOperation.java
@@ -3,7 +3,7 @@ package hexlet.code;
 public class RandomOperation {
     public static final char[] SYMBOLS = {'+', '-', '*'};
     public static char getRandomOperation() {
-        char operation = SYMBOLS[Randomizer.getRandom(0,2)];
+        char operation = SYMBOLS[Randomizer.getRandom(0, 2)];
         return operation;
     }
 }
